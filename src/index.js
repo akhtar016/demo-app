679735aa433591416590104f39b6369022ed4655
Ravi Sharma <mylifetodevelopment@gmail.com>
2020-09-05 13:22:32 +0530

    updated new analytics lib'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Analytics from "npm-vineet";
+import Analytics from "analytics-mirobytes";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 window.analytics = new Analytics("8041d345-dbec-4a36-bf6d-a75d497970ab");
